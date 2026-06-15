99a511bf7302a6a003b3128316d49db940a7d67b
MarceloCarabajal <marce.tsgia@gmail.com>
2026-06-15 19:31:52 -0300

    refactor(dominio): composicion TipoInsumo, Rol, ItemPedido y lista en Pedido

---
+++ dominio/Rol.cs
@@ -0,0 +1,8 @@
+namespace dominio
+{
+    public class Rol
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+    }
+}
---
+++ dominio/TipoInsumo.cs
@@ -0,0 +1,8 @@
+namespace dominio
+{
+    public class TipoInsumo
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+    }
+}
