4eae831e4bc00ac02050e675429e4b06412b0cf2
MarceloCarabajal <marce.tsgia@gmail.com>
2026-06-30 12:39:31 -0300

    feat(negocio): MesaNegocio asignar mesero

--- negocio/MesaNegocio.cs
+++ negocio/MesaNegocio.cs
@@ -152,5 +152,102 @@ namespace negocio
                 datos.cerrarConexion();
             }
         }
+
+        public List<Usuario> listarMeseros()
+        {
+            List<Usuario> lista = new List<Usuario>();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearProcedimiento("sp_listar_meseros");
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Usuario aux = new Usuario();
+                    aux.Id = (int)datos.Lector["Id"];
+                    aux.UserName = (string)datos.Lector["UserName"];
+                    aux.Nombre = (string)datos.Lector["Nombre"];
+                    aux.Apellido = (string)datos.Lector["Apellido"];
+                    aux.Activo = (bool)datos.Lector["Activo"];
+                    aux.Rol = new Rol();
+                    aux.Rol.Id = (int)datos.Lector["IdRol"];
+                    aux.Rol.Nombre = (string)datos.Lector["RolNombre"];
+
+                    lista.Add(aux);
+                }
+
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        public void asignarMesero(int idMesa, int idMesero)
+        {
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearProcedimiento("sp_asignar_mesa_mesero");
+                datos.setearParametro("@IdMesa", idMesa);
+                datos.setearParametro("@IdMesero", idMesero);
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        public List<Mesa> listarAsignadas(int? idMesero = null)
+        {
+            List<Mesa> lista = new List<Mesa>();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearProcedimiento("sp_listar_mesas_asignadas");
+                datos.setearParametro("@IdMesero", idMesero.HasValue ? (object)idMesero.Value : DBNull.Value);
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Mesa aux = new Mesa();
+                    aux.Id = (int)datos.Lector["Id"];
+                    aux.Numero = (int)datos.Lector["Numero"];
+                    aux.Activo = (bool)datos.Lector["Activo"];
+                    aux.EstadoMesa = (string)datos.Lector["EstadoMesa"];
+                    aux.IdMesero = (int)datos.Lector["IdMesero"];
+                    aux.Mesero = (string)datos.Lector["Mesero"];
+
+                    if (datos.Lector["IdPedidoAbierto"] != DBNull.Value)
+                        aux.IdPedidoAbierto = (int)datos.Lector["IdPedidoAbierto"];
+
+                    lista.Add(aux);
+                }
+
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
     }
 }
