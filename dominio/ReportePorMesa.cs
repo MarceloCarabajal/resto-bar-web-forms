3cb15186a09ea8ec82e937fbbb5d7d18fc70198e
MarceloCarabajal <marce.tsgia@gmail.com>
2026-07-01 17:37:44 -0300

    feat(dominio): DTOs reportes por mesa y mesero

---
+++ dominio/ReportePorMesa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class ReportePorMesa
+    {
+        public int NumeroMesa { get; set; }
+        public int CantidadPedidos { get; set; }
+        public decimal TotalVendido { get; set; }
+    }
+}
